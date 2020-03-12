0e6506e49c9f6f4aeef8640870f65d53cedb1173
menvol3 <disbalance@ukr.net>
2020-03-12 16:52:12 +0200

    made changes

--- src/Student.java
+++ src/Student.java
@@ -2,25 +2,35 @@ public class Student {
     private int rating;
     private String name;
 
-    public static int count  = 0 ;
-    public static int countRating  = 0 ;
+    public static int count = 0;
+    public static int countRating = 0;
+
     public Student(String name, int rating) {
         this.rating = rating;
         this.name = name;
-        count ++;
+        count++;
         countRating += rating;
     }
-    public Student() {count ++;
+
+    public Student() {
+        count++;
     }
-  public static void main(String[] args) {
-    Student student1 = new Student("Mykola", 15  );
-    Student student2 = new Student("Oleksandr", 5);
-    Student student3 = new Student("Alex", 50);
 
-  }
+    public static void main(String[] args) {
+        Student student1 = new Student("Mykola", 15);
+        Student student2 = new Student("Oleksandr", 5);
+        Student student3 = new Student("Alex", 50);
+
+        System.out.println(getAvgRating());
+        student1.setRating(99);
+        System.out.println(getAvgRating());
+    }
+
     public static double getAvgRating() {
-
-      return (double) countRating/count;
+        if (countRating == 0) {
+            return 0;
+        } else
+            return (double) countRating / count;
     }
 
     public String getName() {
@@ -36,22 +46,23 @@ public class Student {
     }
 
     public void setRating(int rating) {
-        countRating -= this.rating;
         countRating += rating;
         this.rating = rating;
     }
+
     public boolean betterStudent(Student student) {
         return this.rating > student.getRating();
     }
 
-  public void changeRating(int rating) {
-     setRating(rating);
-  }
+    public void changeRating(int rating) {
+        countRating -= this.rating;
+        setRating(rating);
+    }
 
-  public static void removeStudent(Student student) {
-        int n = Student.count;
-        n = n - 1;
-   }
+    public static void removeStudent(Student student) {
+        count--;
+        countRating -= student.rating;
+    }
 
     @Override
     public String toString() {
